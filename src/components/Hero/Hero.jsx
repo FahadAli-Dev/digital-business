9e91fd1e87d44419566ea22e58853b96f7581844
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-08-07 13:14:51 +0500

    complete disktop version of hero-section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,52 +4,53 @@ import { heroData } from "../../utils/data";
 import Image from "next/image";
 
 const Hero = () => {
-  console.log();
   return (
     <div className={style["hero-wrapper"]}>
-      <div className={style["hero-container"]}>
-        <div className={style["hero-left"]}>
-          {heroData.map((v, i) => {
-            return (
-              <div
-                key={i}
-                style={{
-                  backgroundColor: `${v.bg}`,
-                  margin: `${i == 1 || i == 4 ? "-4rem 0 0 0.1rem" : ""}`,
-                }}
-                className={style["img-container"]}
-              >
-                <Image
-                  className={style["hero-img"]}
-                  src={v.src}
-                  quality={100}
-                  fill
-                  sizes="250px"
-                  alt="img"
+      <div className={style.container}>
+        <div className={style["hero-container"]}>
+          <div className={style["hero-left"]}>
+            {heroData.map((v, i) => {
+              return (
+                <div
+                  key={i}
                   style={{
-                    objectFit: "cover",
-                    marginTop: "2.1rem",
-                    transform: "scale(1.2)",
-                    overflow: "hidden",
+                    backgroundColor: `${v.bg}`,
+                    margin: `${i == 1 || i == 4 ? "-5rem 0 0 0.1rem" : ""}`,
                   }}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className={style["hero-right"]}>
-          <div className={style["hero-title"]}>
-            <span>Redefine How</span>
-            <span>You grow your</span>
-            <span>Digital Business</span>
+                  className={style["img-container"]}
+                >
+                  <Image
+                    className={style["hero-img"]}
+                    src={v.src}
+                    quality={100}
+                    fill
+                    sizes="240px"
+                    alt="img"
+                    style={{
+                      objectFit: "cover",
+                      marginTop: "2.1rem",
+                      transform: "scale(1.2)",
+                      overflow: "hidden",
+                    }}
+                  />
+                </div>
+              );
+            })}
           </div>
-          <div className={style["hero-description"]}>
-            <p>
-              Revenue based funding and execution support designed for early
-              stage founders
-            </p>
+          <div className={style["hero-right"]}>
+            <div className={style["hero-title"]}>
+              <span>Redefine How</span>
+              <span>You grow your</span>
+              <span>Digital Business</span>
+            </div>
+            <div className={style["hero-description"]}>
+              <p>
+                Revenue based funding and execution support designed for early
+                stage founders
+              </p>
+            </div>
+            <EmailBox />
           </div>
-          <EmailBox />
         </div>
       </div>
     </div>
