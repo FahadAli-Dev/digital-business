3a39bdf8309bbf7a8fbbdd0d02421a9a57808e39
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-08-07 22:07:29 +0500

    complete animaiton of hero-right

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
+"use client";
+
 import EmailBox from "../EmailBox/EmailBox";
 import style from "./Hero.module.css";
 import { heroData } from "../../utils/data";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const Hero = () => {
+  const MotionImge = motion(Image);
   return (
     <div className={style["hero-wrapper"]}>
       <div className={style.container}>
@@ -11,29 +15,63 @@ const Hero = () => {
           <div className={style["hero-left"]}>
             {heroData.map((v, i) => {
               return (
+                // <div key={i} style={{ overflow: "hidden" }}>
                 <div
                   key={i}
                   style={{
-                    backgroundColor: `${v.bg}`,
+                    overflow: "hidden",
                     margin: `${i == 1 || i == 4 ? "-5rem 0 0 0.1rem" : ""}`,
                   }}
                   className={style["img-container"]}
                 >
-                  <Image
-                    className={style["hero-img"]}
-                    src={v.src}
-                    quality={100}
-                    fill
-                    sizes="240px"
-                    alt="img"
-                    style={{
-                      objectFit: "cover",
-                      marginTop: "2.1rem",
-                      transform: "scale(1.2)",
-                      overflow: "hidden",
+                  <motion.div
+                    initial={{
+                      y: "18rem",
                     }}
-                  />
+                    animate={{
+                      y: "0rem",
+
+                      transition: {
+                        damping: 25,
+                        type: "spring",
+                      },
+                    }}
+                    style={{
+                      backgroundColor: `${v.bg}`,
+                      borderRadius: "999px",
+                      width: "100%",
+                      height: "100%",
+                    }}
+                  >
+                    <MotionImge
+                      initial={{
+                        y: "18rem",
+                      }}
+                      animate={{
+                        y: "0rem",
+
+                        transition: {
+                          damping: 25,
+                          type: "spring",
+                          // delay: 1,
+                        },
+                      }}
+                      className={style["hero-img"]}
+                      src={v.src}
+                      quality={100}
+                      fill
+                      sizes="240px"
+                      alt="img"
+                      style={{
+                        objectFit: "cover",
+                        marginTop: "2.1rem",
+                        transform: "scale(1.2)",
+                        overflow: "hidden",
+                      }}
+                    />
+                  </motion.div>
                 </div>
+                // </div>
               );
             })}
           </div>
