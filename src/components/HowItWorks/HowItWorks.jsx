ed089b24de8e2f10f54267f6b2d0c8fbed1f91c2
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-08-13 22:49:08 +0500

    complete WhatWeDo section for all screens and also animation

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,30 +1,68 @@
+"use client";
+
 import style from "@/src/components/HowItWorks/HowItWorks.module.css";
 import { HowItWork } from "@/src/utils/data";
 import Image from "next/image";
+import { motion, scale } from "motion/react";
+import { tagVariants, titleVariants } from "@/src/utils/animation";
+
+const featureVariants = {
+  offscreen: { scale: 0.5 },
+  onscreen: {
+    scale: 1,
+    transition: { duration: 1.5, type: "spring" },
+  },
+};
 
 const HowItWorks = () => {
   return (
     <div className={style["hiw-wrapper"]}>
       <div className={style["hiw-container"]}>
         <div className={style["hiw-head"]}>
-          <span className="tag">How it works</span>
-          <span className="title">
+          <motion.span
+            className="tag"
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={tagVariants}
+          >
+            How it works
+          </motion.span>
+          <motion.span
+            className="title"
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={titleVariants}
+          >
             Unlocking potential along the growth journey
-          </span>
+          </motion.span>
         </div>
         <div className={style["hiw-features"]}>
           {HowItWork.map((fea, idx) => {
             return (
-              <div className={style["hiw-feature"]} key={idx}>
-                <div className={style["detail"]}>
+              <motion.div
+                className={style["hiw-feature"]}
+                key={idx}
+                initial="offscreen"
+                whileInView="onscreen"
+                variants={featureVariants}
+              >
+                <motion.div
+                  className={style["detail"]}
+                  initial={{ opacity: 0, x: -100 }}
+                  whileInView={{
+                    opacity: 1,
+                    x: 0,
+                    transition: { type: "easeIn", duration: 1, delay: 0.7 },
+                  }}
+                >
                   <span className="des">0{idx + 1}</span>
                   <span className="sec-title">{fea.title}</span>
                   <span className="text">{fea.des}</span>
-                </div>
+                </motion.div>
                 <div className={style["icon"]}>
                   <Image src={fea.icon} alt="img" width={128} height={128} />
                 </div>
-              </div>
+              </motion.div>
             );
           })}
         </div>
